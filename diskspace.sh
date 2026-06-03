4e8e641e1fba76a2e432b89068f6260f108a9027
SMILEX117 <vitalynamm114@gmail.com>
2026-06-03 08:49:48 +0000

    Diskspace-Skript fertiggestellt

--- diskspace.sh
+++ diskspace.sh
@@ -1,1 +1,6 @@
-df -h
+#!/bin/bash
+echo "--- Speicherplatz der Festplatten ---"
+df -h -x tmpfs -x devtmpfs
+echo ""
+echo "--- Größte Dateien im aktuellen Ordner ---"
+du -sh * 2>/dev/null | sort -hr | head -n 5
