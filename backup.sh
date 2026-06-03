1bc797f87d15fcfbdf39cc97aceea477dc74300f
SMILEX117 <vitalynamm114@gmail.com>
2026-06-03 08:10:53 +0000

    Professionelles Backup-Skript erstellt

--- backup.sh
+++ backup.sh
@@ -1,1 +1,12 @@
-tar -czvf backup.tar.gz /home
+#!/bin/bash
+QUELL_ORDNER="/home/vboxuser/linux-course"
+ZIEL_DATEI="/tmp/backup_$(date +%Y%m%d_%H%M).tar.gz"
+
+echo "Starte Backup..."
+tar -czf "$ZIEL_DATEI" "$QUELL_ORDNER" 2>/dev/null
+
+if [ $? -eq 0 ]; then
+    echo "Erfolg! Gespeichert als: $ZIEL_DATEI"
+else
+    echo "Fehler beim Backup!"
+fi
